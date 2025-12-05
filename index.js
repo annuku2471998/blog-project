2cb3c437083515d620fe265b936a6e3547500fe6
annuku2471998 <annuku2471998@gmail.com>
2025-12-05 18:01:28 +0530

    add css file and make home page by using index.ejs file

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 
 import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.set("view engine", "ejs");
+
 
 app.get("/", (req, res) => {
-    res.send("haloo i am root...");
-})
+    res.render("index.ejs");
+});
 
 
 app.listen(3030, () => {
