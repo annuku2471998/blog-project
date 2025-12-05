7a72478a511e1753b63535f4ae3dd2e00ce598d6
annuku2471998 <annuku2471998@gmail.com>
2025-12-05 16:17:39 +0530

    define home route in express

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 
 import express from "express";
 
+
 const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("haloo i am root...");
+})
+
 
 app.listen(3030, () => {
      console.log("port is start..");
-})
+});
